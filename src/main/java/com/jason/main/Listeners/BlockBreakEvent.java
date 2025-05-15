7587dc4a8359e2a3ab5cb12b988a1cc93edb4173
Kirk-KD <kirk.ding123@gmail.com>
2025-05-14 20:08:18 -0400

    try resolve conflict

---
+++ src/main/java/com/jason/main/Listeners/BlockBreakEvent.java
@@ -0,0 +1,11 @@
+package com.jason.main.Listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class BlockBreakEvent implements Listener {
+    @EventHandler
+    public static void onBlockBreak(org.bukkit.event.block.BlockBreakEvent event) {
+
+    }
+}
