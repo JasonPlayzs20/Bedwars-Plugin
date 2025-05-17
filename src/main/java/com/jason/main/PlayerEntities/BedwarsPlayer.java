3ca2af1e62bbd1aca7dba047cbfaabc78001edd4
JasonPlayzs20 <103009819+JasonPlayzs20@users.noreply.github.com>
2025-05-17 13:25:18 -0400

    Team and stuff

--- src/main/java/com/jason/main/PlayerEntities/BedwarsPlayer.java
+++ src/main/java/com/jason/main/PlayerEntities/BedwarsPlayer.java
@@ -41,7 +41,27 @@ public class BedwarsPlayer {
 
     public void start() {
         mainSpawn = new Location(world, 0, Double.parseDouble(getData("plugins/BedwarsInfo", name.substring(1) + ".yml", "lobbySpawn.y")) - 30, 0);
-        baseSpawn = new Location(world, Double.parseDouble(getData("plugins/BedwarsInfo", name.substring(1) + ".yml", team.teamColors.name().toString().substring(0, 1).toLowerCase() + ".p.x")), Double.parseDouble(getData("plugins/BedwarsInfo", name.substring(1) + ".yml", team.teamColors.name().toString().substring(0, 1).toLowerCase() + ".p.y")) + 0.5, Double.parseDouble(getData("plugins/BedwarsInfo", name.substring(1) + ".yml", team.teamColors.name().toString().substring(0, 1).toLowerCase() + ".p.z"))).setDirection(new Vector(0,0,-2500));
+        baseSpawn = new Location(world, Double.parseDouble(getData("plugins/BedwarsInfo", name.substring(1) + ".yml", team.teamColors.name().toString().substring(0, 1).toLowerCase() + ".p.x")), Double.parseDouble(getData("plugins/BedwarsInfo", name.substring(1) + ".yml", team.teamColors.name().toString().substring(0, 1).toLowerCase() + ".p.y")) + 0.5, Double.parseDouble(getData("plugins/BedwarsInfo", name.substring(1) + ".yml", team.teamColors.name().toString().substring(0, 1).toLowerCase() + ".p.z")));
+        player.sendMessage(String.valueOf(baseSpawn.getY()));
+        player.sendMessage(String.valueOf(baseSpawn.getX()));
+        if (Math.abs(baseSpawn.getZ()) > Math.abs(baseSpawn.getX())) {
+            if (baseSpawn.getZ() < 0) {
+                player.sendMessage("less than 0 Y");
+//                baseSpawn.setDirection(new Vector(0, 0, 0));
+            } else {
+                player.sendMessage("greater than 0 Y");
+                baseSpawn.setDirection(new Vector(0, 0, -2500));
+            }
+        }else  {
+            if (baseSpawn.getX() < 0) {
+                player.sendMessage("less than 0 X");
+                baseSpawn.setDirection(new Vector(90, 0, 0));
+            } else {
+                player.sendMessage("greater than 0 X");
+                baseSpawn.setDirection(new Vector(-90, 0, 0));
+            }
+        }
+
         player.teleport(baseSpawn);
     }
 
