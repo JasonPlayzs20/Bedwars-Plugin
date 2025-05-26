009453b67b1ffea6aa31bc0d3c524415220ca61d
Kirk-KD <kirk.ding123@gmail.com>
2025-05-25 21:57:55 -0400

    Edge Brigg

--- src/main/java/com/jason/main/Listeners/ProjectileListener.java
+++ src/main/java/com/jason/main/Listeners/ProjectileListener.java
@@ -1,12 +1,15 @@
 package com.jason.main.Listeners;
 
 import com.jason.main.items.BedbugItem;
+import com.jason.main.items.BridgeEgg;
+import org.bukkit.entity.Egg;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.projectiles.ProjectileSource;
 
 public class ProjectileListener implements Listener {
@@ -16,6 +19,15 @@ public class ProjectileListener implements Listener {
         ProjectileSource shooter = projectile.getShooter();
         if (projectile instanceof Snowball && shooter instanceof Player) {
             BedbugItem.summonBedbug((Player) shooter, event.getEntity().getLocation());
+        }
+    }
+
+    @EventHandler
+    public static void onProjectileLaunch(ProjectileLaunchEvent event) {
+        Projectile projectile = event.getEntity();
+        ProjectileSource shooter = projectile.getShooter();
+        if (projectile instanceof Egg && shooter instanceof Player) {
+            BridgeEgg.placeBridgeBlocks((Player) shooter, (Egg) projectile);
         }
     }
 }
---
+++ src/main/java/com/jason/main/items/BridgeEgg.java
@@ -0,0 +1,67 @@
+package com.jason.main.items;
+
+import com.jason.main.GameDisplay.Arenas;
+import com.jason.main.PlayerEntities.BedwarsPlayer;
+import com.jason.main.bedwars;
+import org.bukkit.DyeColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Egg;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.material.Wool;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Edge Brigg
+public class BridgeEgg extends BedwarsItem {
+    public BridgeEgg() {
+        super(Material.EGG, "Bridge Egg");
+    }
+
+    @Override
+    public void onUse(PlayerInteractEvent event) {
+        // let the egg be thrown normally
+    }
+
+    public static void placeBridgeBlocks(Player spawner, Egg egg) {
+        BedwarsPlayer bwPlayer = Arenas.getPlayer(spawner);
+        if (bwPlayer == null) return;
+
+        DyeColor blockColor = DyeColor.valueOf(bwPlayer.getTeam().teamColors.name());
+        Wool coloredWool = new Wool();
+        coloredWool.setColor(blockColor);
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (egg.isDead()) this.cancel();
+
+                World w = spawner.getWorld();
+
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        double x = egg.getLocation().getX();
+                        double y = egg.getLocation().getY();
+                        double z = egg.getLocation().getZ();
+
+                        for (int dx = -1; dx <= 1; dx++) {
+                            for (int dz = -1; dz <= 1; dz++) {
+                                double bx = x + dx;
+                                double bz = z + dz;
+                                Location loc = new Location(w, bx, y, bz);
+                                Block block = w.getBlockAt(loc);
+                                if (block.getType().equals(Material.AIR)) block.setType(coloredWool.getItemType());
+                            }
+                        }
+                    }
+                }.runTaskLater(bedwars.getMainInstance(), 20);
+            }
+        }.runTaskTimer(bedwars.getMainInstance(), 0, 1);
+    }
+}
